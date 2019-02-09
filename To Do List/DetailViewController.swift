ad609fcb56929d0443a5f782b028ac8cf126781f
Joseph <joseph@Josephs-MacBook-Pro.local>
2019-02-09 18:44:09 -0500

    thru 5.2

---
+++ To Do List/DetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DetailViewController.swift
+//  To Do List
+//
+//  Created by Joseph on 2/9/19.
+//  Copyright © 2019 Joseph Parks. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var toDoField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+}
